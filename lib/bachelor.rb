45e0d9b038aab64cf8a32a8884013b3955e6f076
Ben Matare <ben.a.matare@gmail.com>
2018-01-29 07:08:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -79,5 +79,5 @@ def get_average_age_for_season(data, season)
       end
     end
   end
-holder_array.inject(0.0) { |x, y| x + 1y}.round / holder_array.size
+holder_array.inject(0.0) { |x, y| x + 1 + y}.round / holder_array.size
 end
